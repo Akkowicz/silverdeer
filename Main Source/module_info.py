a11859d7e8d78b321680df911af8caf65d63732b
akkowicz <uamfhq@gmail.com>
2018-05-24 18:50:45 +0200

    Rebrand

--- Main Source/module_info.py
+++ Main Source/module_info.py
@@ -3,5 +3,5 @@
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
 #export_dir = "/home/pavol/Mount&Blade Warband Modules/silverstag-dev/"
-export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Silverstag (Cinematic)/"
+export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Silverdeer/"
 #export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
